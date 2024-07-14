129cd32c292cf5532f6958b8223870488bc7e904
Firmiana <122654548+FirmianaI7z9@users.noreply.github.com>
2024-07-14 17:33:18 +0900

    [Add] mypage.php

---
+++ mypage.php
@@ -0,0 +1,100 @@
+<?php
+  /* LOCAL */
+  require_once "C:/Users/ftech/Documents/Github/ISWAO/php/default.php";
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <meta name="description" content="学術オリンピックや競技科学に関する様々な情報をまとめた非公式サイトのホームページ">
+  <meta property="og:url" content="https://acaoly-inofficial.com/index.php">
+  <meta property="og:type" content="website">
+  <meta property="og:site_name" content="学術オリンピック非公式まとめサイト">
+  <meta property="og:image" content="https://acaoly-inofficial.com/img/comp_site_thumbnail.png">
+  <meta property="og:title" content="ホーム">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="@acaoly_notifi">
+  <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto+Condensed:wght@500&display=swap" rel="stylesheet">
+  <title>ホーム | 学術オリンピック非公式まとめサイト</title>
+  <link rel="stylesheet" href="/css/common.css">
+  <link rel="stylesheet" href="/css/colors.css">
+  <link rel="stylesheet" href="/css/editorial.css">
+  <link rel="stylesheet" href="/css/past.css">
+  <script src=""></script>
+</head>
+
+<body>
+  <?php echo default_header(); ?>
+  <h2 class="h2">My Page</h2>
+  <div class="basic-container">
+    <p class="username-large" style="color:#0078b8;">Firmiana</p>
+    <p class="user-belongs">Tokushima Municipal High School (The University of Tokyo), 運営</p>
+    <hr>
+    <div class="user-profile-container">
+      <div class="user-profile-item">
+        <h3 class="user-profile-item-title">Basic Data</h3>
+        <table class="user-profile-item-table">
+          <tbody>
+            <tr>
+              <th>誕生年</th>
+              <td>2005</td>
+            </tr>
+            <tr>
+              <th>参加大会数</th>
+              <td>11 (19)</td>
+            </tr>
+            <tr>
+              <th>参加経験</th>
+              <td>
+                <span class="additional-tag-wrapper background-gray color-white"><span class="additional-tag-innertag background-mathematics color-black">JMO</span>yo</span><span class="additional-tag-wrapper background-gray color-white"><span class="additional-tag-innertag background-mathematics color-black">JJMO</span>yo</span><span class="additional-tag-wrapper background-darkgray color-white"><span class="additional-tag-innertag background-physics color-white">JPhO</span>ho</span><span class="additional-tag-wrapper background-darkgray color-white"><span class="additional-tag-innertag background-chemistry color-black">JChO</span>ho</span><span class="additional-tag-wrapper background-gray color-white"><span class="additional-tag-innertag background-earthscience color-black">JESO</span>yo</span><span class="additional-tag-wrapper background-gray color-white"><span class="additional-tag-innertag background-informatics color-white">JOI</span>yo</span><span class="additional-tag-wrapper background-darkgray color-white"><span class="additional-tag-innertag background-kako color-white">科甲</span>ho</span><span class="additional-tag-wrapper background-darkgray color-white"><span class="additional-tag-innertag background-kako color-white">科甲Jr</span>ho</span><span class="tag background-linguistics color-black">JOL</span><span class="tag background-linguistics color-black">Onling</span><span class="additional-tag-wrapper background-darkgray color-white"><span class="additional-tag-innertag background-astronomy color-white">JAO</span>ho</span>
+              </td>
+            </tr>
+            <tr>
+              <th>解説作成数</th>
+              <td>0</td>
+            </tr>
+            <tr>
+              <th>Twitter</th>
+              <td>@FirmianaSim4104</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="basic-container">
+    <div class="user-profile-container">
+      <div class="user-profile-item">
+        <h3 class="user-profile-item-title">User Editorials</h3>
+        <div class="editorial-link-container">
+          <div class="editorial-link">
+            <a href=""></a>
+            <p class="editorial-link-title">JMO2024yo-P1</p>
+            <p class="editorial-link-data"><span class="additional-tag-wrapper background-gray color-white"><span class="additional-tag-innertag background-mathematics color-black">JMO</span>yo</span> by <span class="username" style="color:#0078b8">Firmiana</span> (確認中)</p>
+          </div>
+          <div class="editorial-link">
+            <a href=""></a>
+            <p class="editorial-link-title">JMO2024yo-P2</p>
+            <p class="editorial-link-data"><span class="additional-tag-wrapper background-gray color-white"><span class="additional-tag-innertag background-mathematics color-black">JMO</span>yo</span> by <span class="username" style="color:#0078b8">Firmiana</span> (未公開)</p>
+          </div>
+          <div class="editorial-link">
+            <a href=""></a>
+            <p class="editorial-link-title">JMO2024yo-P3</p>
+            <p class="editorial-link-data"><span class="additional-tag-wrapper background-gray color-white"><span class="additional-tag-innertag background-mathematics color-black">JMO</span>yo</span> by <span class="username" style="color:#0078b8">Firmiana</span> (未公開)</p>
+          </div>
+        </div>
+        
+        <p>まだ解説を作成していません。解説の作成は、各問題のページから行うことができます。</p>
+      </div>
+    </div>
+  </div>
+  <?php echo default_footer(); ?>
+</body>
+
+</html>
