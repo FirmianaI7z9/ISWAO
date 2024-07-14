cd4a64f3482c6167b0b977d4d0362997ed9252df
Firmiana <122654548+FirmianaI7z9@users.noreply.github.com>
2024-07-14 17:33:28 +0900

    [Add] about.php

---
+++ about.php
@@ -0,0 +1,44 @@
+<?php
+  /* LOCAL */
+  require_once "C:/Users/ftech/Documents/Github/ISWAO/php/default.php";
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <meta name="description" content="学術オリンピックや競技科学に関する様々な情報をまとめた非公式サイトのホームページ">
+  <meta property="og:url" content="https://acaoly-inofficial.com/index.php">
+  <meta property="og:type" content="website">
+  <meta property="og:site_name" content="学術オリンピック非公式まとめサイト">
+  <meta property="og:image" content="https://acaoly-inofficial.com/img/comp_site_thumbnail.png">
+  <meta property="og:title" content="ホーム">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="@acaoly_notifi">
+  <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto+Condensed:wght@500&display=swap" rel="stylesheet">
+  <title>ホーム | 学術オリンピック非公式まとめサイト</title>
+  <link rel="stylesheet" href="/css/common.css">
+  <link rel="stylesheet" href="/css/colors.css">
+  <link rel="stylesheet" href="/css/information.css">
+  <link rel="stylesheet" href="/css/past.css">
+  <script src=""></script>
+</head>
+
+<body>
+  <?php echo default_header(); ?>
+  <h2 class="h2">About</h2>
+
+  <div class="basic-container">
+    <p class="basic-text">このウェブサイト「学術オリンピック非公式まとめサイト (ISWAO; Inofficial Summary Website of Academic Olympiad)」は、より多くの人が競技科学を知ることができるように、より多くの競技に多くの人が参加できるように、そして今後ますます学術オリンピック・競技科学の世界が発展できるように、その一助となることを目指して運営されているものです。</p>
+    <p class="basic-text">運営は、どこにでもいる大学生の<span class="username" style="color:#0078b8;">Firmiana</span>が自費で行っています。至らないところも多いと思いますが、何卒よろしくお願いします。</p>
+  </div>
+
+  <?php echo default_footer(); ?>
+</body>
+
+</html>
