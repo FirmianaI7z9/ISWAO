c25721adbe653e8cc11fd7447d798bab0cc428d6
Firmiana <122654548+FirmianaI7z9@users.noreply.github.com>
2024-07-22 21:49:38 +0900

    [Update] about.php

--- about.php
+++ about.php
@@ -35,7 +35,14 @@
 
   <div class="basic-container">
     <p class="basic-text">このウェブサイト「学術オリンピック非公式まとめサイト (ISWAO; Inofficial Summary Website of Academic Olympiad)」は、より多くの人が競技科学を知ることができるように、より多くの競技に多くの人が参加できるように、そして今後ますます学術オリンピック・競技科学の世界が発展できるように、その一助となることを目指して運営されているものです。</p>
-    <p class="basic-text">運営は、どこにでもいる大学生の<span class="username" style="color:#0078b8;">Firmiana</span>が自費で行っています。至らないところも多いと思いますが、何卒よろしくお願いします。</p>
+    <p class="basic-text">運営は、どこにでもいる大学生の <span class="username" style="color:#0078b8;">Firmiana</span> が自費で行っています。至らないところも多いと思いますが、何卒よろしくお願いします。</p>
+    <p class="basic-text">私の趣味として運営している側面もありますので、今のところ一切の支援はお受けしない方針です。また、本ウェブサイト及び関連する Twitter アカウントの収益化も一切行っていませんし、今後するつもりもありません。さらに、現状共同運営者の募集もしておりません。ご理解のほどよろしくお願いします。</p>
+    <hr>
+    <p class="basic-text">注：以下は全て運営用のアカウントです。またこれ以外のアカウントは運営が保有しているものではありません。</p>
+    <p class="basic-text"><span class="username" style="color:#0078b8;">Firmiana</span>、<span class="username" style="color:#0078b8;">Firmiana2</span>、<span class="username" style="color:#000000;">admin</span></p>
+    <hr>
+    <p class="basic-text">以下は1問以上解説を書いたユーザーの皆様です。ご協力ありがとうございます。</p>
+    <p class="basic-text"><span class="username" style="color:#0078b8;">Firmiana</span></p>
   </div>
 
   <?php echo default_footer(); ?>
