45f6e974e58b3a87cf1f67f06174ed0939bf4e19
Firmiana <122654548+FirmianaI7z9@users.noreply.github.com>
2024-08-03 20:38:30 +0900

    [Update] Update meta info of schedule.php

--- schedule.php
+++ schedule.php
@@ -23,14 +23,14 @@
   <meta property="og:type" content="website">
   <meta property="og:site_name" content="学術オリンピック非公式まとめサイト">
   <meta property="og:image" content="https://acaoly-inofficial.com/img/comp_site_thumbnail.png">
-  <meta property="og:title" content="ホーム">
+  <meta property="og:title" content="スケジュール">
   <meta name="twitter:card" content="summary">
   <meta name="twitter:site" content="@acaoly_notifi">
   <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto+Condensed:wght@500&display=swap" rel="stylesheet">
-  <title>ホーム | 学術オリンピック非公式まとめサイト</title>
+  <title>スケジュール | 学術オリンピック非公式まとめサイト</title>
   <link rel="stylesheet" href="/css/common.css">
   <link rel="stylesheet" href="/css/colors.css">
   <link rel="stylesheet" href="/css/schedule.css">
