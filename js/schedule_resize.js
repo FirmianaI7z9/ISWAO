2f8b359f9c9c8a9d6fe855da4c98c07976447b7e
Firmiana <122654548+FirmianaI7z9@users.noreply.github.com>
2024-08-03 20:38:01 +0900

    [Fix] Fix some bugs on schedule_resize.js

--- js/schedule_resize.js
+++ js/schedule_resize.js
@@ -8,6 +8,7 @@ window.addEventListener('resize', function() {
 
 function resizewidth() {
   var items = document.getElementsByClassName('schedule-right-text');
+  items[0].style = "";
   var size = window.getComputedStyle(items[0]);
   var basewidth = Number(size.width.substring(0, size.width.length - 2));
   var canvas = document.createElement('canvas');
@@ -16,6 +17,6 @@ function resizewidth() {
   for (let i = 0; i < items.length; i++) {
     var metrics = context.measureText(items[i].innerText);
     var width = metrics.width;
-    items[i].style = "width: " + (Math.max(100, width / basewidth * 100 + 5)).toFixed(2) + "%; transform: scaleX(" + (Math.min(1.0000, basewidth / width)).toFixed(4) + ");";
+    items[i].style = "width: " + (Math.max(100, width * 1.0 / basewidth * 100) + 1).toFixed(2) + "%; transform: scaleX(" + (Math.min(1.0000, basewidth * 0.99 / width)).toFixed(4) + ");";
   }
 }
