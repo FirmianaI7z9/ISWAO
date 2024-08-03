a60e2713bf8ec1100cb3fd372ec271cf02d52f3e
Firmiana <122654548+FirmianaI7z9@users.noreply.github.com>
2024-08-03 20:36:20 +0900

    [Add] Create schedule editor

--- dev/schedule_a.php
+++ dev/schedule_a.php
@@ -11,12 +11,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto+Condensed:wght@500&display=swap" rel="stylesheet">
   <title>ホーム | 学術オリンピック非公式まとめサイト</title>
   <link rel="stylesheet" href="/css/common.css">
-  <link rel="stylesheet" href="/css/colors.css">
   <link rel="stylesheet" href="/css/past.css">
   <link rel="stylesheet" href="/css/develop.css">
 </head>
@@ -24,6 +20,7 @@
 <body>
   <?php echo default_header(); ?>
   <h2 class="h2">Developer Page</h2>
+  <a href="index.php">Back to home</a>
 
   <div class="basic-container">
     <h3 class="basic-h3">Add Schedule</h3>
@@ -258,21 +255,21 @@
       var val = { "table" : "schedules", "arg" : [] };
       var dit = document.getElementsByClassName('dit');
       var set = 9;
-      for (let i = 0; i < dit.length / 9; i++) {
-        if (dit[i * 9 + 0].value == '') continue;
+      for (let i = 0; i < dit.length / set; i++) {
+        if (dit[i * set + 0].value == '') continue;
         var item = {
-          "title" : dit[i * 9 + 0].value,
-          "genre" : dit[i * 9 + 1].value,
-          "tags" : JSON.stringify(dit[i * 9 + 2].value.split(',').map((x) => {
+          "title" : dit[i * set + 0].value,
+          "genre" : dit[i * set + 1].value,
+          "tags" : JSON.stringify(dit[i * set + 2].value.split(',').map((x) => {
             var y = x.split(' ');
             return { 'type' : y[0] , 'name' : y[1] , 'kind' : y[2] };
           })),
-          "event_type" : Number(dit[i * 9 + 3].value),
-          "display_type" : Number(dit[i * 9 + 4].value),
-          "start_at" : dit[i * 9 + 5].value,
-          "finish_at" : dit[i * 9 + 6].value,
-          "is_link" : Number(dit[i * 9 + 7].value),
-          "url" : dit[i * 9 + 8].value
+          "event_type" : Number(dit[i * set + 3].value),
+          "display_type" : Number(dit[i * set + 4].value),
+          "start_at" : dit[i * set + 5].value,
+          "finish_at" : dit[i * set + 6].value,
+          "is_link" : Number(dit[i * set + 7].value),
+          "url" : dit[i * set + 8].value
         };
         val["arg"].push(item);
       }
