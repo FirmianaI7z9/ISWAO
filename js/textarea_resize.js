1e31768552f9e49d6af0016841f46b44b7dd47dc
Firmiana <122654548+FirmianaI7z9@users.noreply.github.com>
2024-09-18 22:39:02 +0900

    Create textarea_resize.js

---
+++ js/textarea_resize.js
@@ -0,0 +1,14 @@
+function flexTextarea(el) {
+  const dummy = el.querySelector('.edit-textarea-dummy');
+  el.querySelector('.edit-textarea').addEventListener('input', e => {
+    dummy.textContent = e.target.value + '\u200b';
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  var items = document.getElementsByClassName("edit-textarea-container");
+  console.log(items.length);
+  for (let i = 0; i < items.length; i++) {
+    flexTextarea(items[i]);
+  }
+});
